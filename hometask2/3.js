5c27710b8ee5bfc92450a682ab987ed4efbb8bc2
Ilua Ivanov <iluaivanov@MacBook-Pro-Ilua.local>
2021-03-03 22:36:53 +0200

    added 3.js

---
+++ hometask2/3.js
@@ -0,0 +1,14 @@
+let number, n, counter;
+number = 20; 
+n = 5; 
+counter = 0;
+
+for(let i = 0; i < number; i++){ 
+    if(counter == n){
+        break;
+    }
+    if( number % i == 0 ){
+        console.log(i);
+        counter++;
+    }
+}
